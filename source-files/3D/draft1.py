f1c5cf233756f5a8a1d070d5c07c632313f073b8
Denis Davydov <denis.davydov@nixsolutions.com>
2021-09-10 08:21:08 +0300

    Задание8.1 - двигаемся

--- source-files/3D/draft1.py
+++ source-files/3D/draft1.py
@@ -1,5 +1,3 @@
-
-
 # with open('D:\\Python\\python_1M_2O_3D\\2O\\lessons\\hello1.txt','a') as myfile:
 #     myfile.write('\n\t Hi Den')
 #     print("\n string 3", file=myfile)
@@ -15,14 +13,22 @@
 Текст ввести в файл TEXT.TXT. Результаты работы программы записать в файл REZULT.TXT.
 8.1. Поменяйте в каждом предложении текста первое слово с последним.
 '''
-s = "Hello.I'm so happy to see you."
+import re
+
 with open('text.txt', 'w') as myfile:
+    s = "Hello.I'm so happy to- see you,lets go! to the cinema"
     myfile.write(s)
 
 with open('text.txt', 'r') as myfile:
-    #lst = s.split() ["Hello.I'm", 'so', 'happy', 'to', 'see', 'you.'] #split() возвращает список строк.
-    lst = s.replace('.', '').split() #["HelloI'm", 'so', 'happy', 'to', 'see', 'you']
+    str_clean = re.sub("[,|.|!|-]", " ", myfile.readline())
+    lst = str_clean.split()
     print(lst)
-    lst[0],lst[-1]=lst[-1],lst[0]
+    k = lst[-1].pop
+    lst.append(lst[0])
+    lst.insert(0,k)
+    #lst[0], lst[-1] = lst[-1], lst[0]
     print(lst)
 
+print('------------------------------------------------------')
+
+print('-------------------------------------------------------')
